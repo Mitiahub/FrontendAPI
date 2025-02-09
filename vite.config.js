62065b030b21d29e3e2737ada7abb132a47274b8
VotreNom <votre.email@example.com>
2025-02-09 20:51:10 +0300

    09

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Change le chemin des assets pour fonctionner sur ByetHost
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    assetsDir: 'assets', // Organisation des fichiers dans /dist/assets/
+    outDir: 'dist', // Assure que le build est bien généré dans /dist
   },
 })
